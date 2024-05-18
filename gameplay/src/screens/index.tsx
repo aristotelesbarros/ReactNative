4224bc0cbf580e1f3683d0b3824ec6b700b27175
Aristoteles Barros Dal Farra <143902784+aristotelesbarros@users.noreply.github.com>
2024-05-18 17:31:40 -0300

    AULA 1

---
+++ gameplay/src/screens/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import{View, Text} from 'react-native'
+import { styles } from "./styles";
+
+export  function Signin(){
+  return(
+
+    <View style = {styles.caixa}>
+    <Text>
+
+    HAUHSUAHSUAHUA
+    </Text>
+
+    </View>
+
+  );
+}
---
+++ gameplay/src/screens/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    caixa:{
+        flex:1,
+        backgroundColor:'#B7D5E5'
+    }
+})
